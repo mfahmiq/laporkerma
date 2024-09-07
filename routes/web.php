4be8385c63c9539c54714a4d029d5e515fac4e0a
Akur <ade.kur.gntg@gmail.com>
2024-09-07 15:08:54 +0700

    Fix Conflict

--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,53 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MitraController;
 use App\Http\Controllers\RegisterController;
 
+// Route untuk halaman utama
 Route::get('/', function () {
     return view('login/index');
-=======
+});
 
+// Route untuk halaman login
 Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/', function () {
-    return view('dashboard');
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
-});
-
+// Route untuk halaman kerma
 Route::get('/kerma', function () {
     return view('kerjasama/kerma');
 });
 
+// Route untuk membuat kerma
 Route::get('/kerma/create', function () {
     return view('kerjasama/kermaCreate');
 })->name('kerma.create');
 
+// Route untuk mengedit kerma
 Route::get('/kerma/edit', function () {
     return view('kerjasama/kermaEdit');
 })->name('kermaEdit');
 
-<<<<<<< HEAD
-=======
-Route::get('/mitra', function () {
-    return view('mitra/mitra');
-});
-
-
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
+// Route profile
 Route::get('/profile', function () {
     return view('profil/profile');
 });
 
-<<<<<<< HEAD
+// Route login menggunakan controller
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
+// Route register menggunakan controller
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
+// Route dashboard
 Route::get('/dashboard', function() {
     return view('dashboard.index');
 })->middleware('auth');
 
+// Resource route untuk mitra
 Route::resource('/mitra', MitraController::class)->middleware('auth');
-=======
-
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
