bd6d88dc7ef8172606f4d4bf0925e871c0d01483
Akur <ade.kur.gntg@gmail.com>
2024-09-07 15:58:50 +0700

    Fix Conflict

--- resources/views/mitra/mitraCreate.blade.php
+++ resources/views/mitra/mitraCreate.blade.php
@@ -6,7 +6,6 @@
                 <button type="button" class="btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-<<<<<<< HEAD
                 <form action="/mitra" method="post">
                     @csrf
                     <div class="mb-3">
@@ -15,33 +14,18 @@
                             data-placeholder="Pilih Klasifikasi Mitra">
                             <option></option>
                             <option value="1">Pilihan 1</option>
-=======
-                <form>
-                    <div class="mb-3">
-                        <label for="klasifikasi" class="form-label">Klasifikasi Mitra</label>
-                        <select class="form-select select2" id="klasifikasi" data-placeholder="Pilih Klasifikasi Mitra">
-                            <option></option>
-                            <option value="1">Pilihan 1</option>
-                            <option value="2">Pilihan 2</option>
-                            <option value="3">Pilihan 3</option>
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                         </select>
                     </div>
                     <div class="mb-3">
                         <label for="nama_institusi" class="form-label">Nama Institusi</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-building"></i></span>
-<<<<<<< HEAD
                             <input type="text" class="form-control" id="nama_institusi" name="nama_institusi"
                                 placeholder="Nama Institusi">
-=======
-                            <input type="text" class="form-control" id="nama_institusi" placeholder="Nama Institusi">
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="alamat" class="form-label">Alamat</label>
-<<<<<<< HEAD
                         <textarea class="form-control" id="alamat" name="alamat" rows="3"></textarea>
                     </div>
                     <div class="mb-3">
@@ -50,36 +34,20 @@
                             data-placeholder="Pilih Negara">
                             <option></option>
                             <option value="ID">Indonesia</option>
-=======
-                        <textarea class="form-control" id="alamat" rows="3"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="negara" class="form-label">Negara</label>
-                        <select class="form-select select2" id="negara" data-placeholder="Pilih Negara">
-                            <option></option>
-                            <option value="ID">Indonesia</option>
-                            <option value="US">Amerika Serikat</option>
-                            <option value="AU">Australia</option>
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                         </select>
                     </div>
                     <div class="mb-3">
                         <label for="telp" class="form-label">Telp.</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-telephone"></i></span>
-<<<<<<< HEAD
                             <input type="text" class="form-control" id="telp" name="telp"
                                 placeholder="Nomor Telepon">
-=======
-                            <input type="text" class="form-control" id="telp" placeholder="Nomor Telepon">
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="website" class="form-label">Website</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-globe"></i></span>
-<<<<<<< HEAD
                             <input type="text" class="form-control" id="website" name="website"
                                 placeholder="Alamat Website">
                         </div>
@@ -90,17 +58,6 @@
                     </div>
                 </form>
             </div>
-=======
-                            <input type="text" class="form-control" id="website" placeholder="Alamat Website">
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-success btn-sm"><i class="bx bx-save"></i> Simpan</button>
-                <button type="button" class="btn btn-light btn-sm" data-bs-dismiss="modal">Close</button>
-            </div>
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
         </div>
     </div>
 </div>
--- resources/views/mitra/mitraEdit.blade.php
+++ resources/views/mitra/mitraEdit.blade.php
@@ -6,11 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-<<<<<<< HEAD
                 <form action="#" method="post">
-=======
-                <form>
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                     <div class="mb-3">
                         <label for="klasifikasi" class="form-label">Klasifikasi Mitra</label>
                         <select class="form-select select2" id="klasifikasi" data-placeholder="Pilih Klasifikasi Mitra">
@@ -24,21 +20,13 @@
                         <label for="nama_institusi" class="form-label">Nama Institusi</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-building"></i></span>
-<<<<<<< HEAD
                             <input type="text" class="form-control" id="nama_institusi" name="nama_institusi"
                                 placeholder="Nama Institusi">
-=======
-                            <input type="text" class="form-control" id="nama_institusi" placeholder="Nama Institusi">
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="alamat" class="form-label">Alamat</label>
-<<<<<<< HEAD
                         <textarea class="form-control" id="alamat" name="alamat" rows="3"></textarea>
-=======
-                        <textarea class="form-control" id="alamat" rows="3"></textarea>
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                     </div>
                     <div class="mb-3">
                         <label for="negara" class="form-label">Negara</label>
@@ -53,19 +41,14 @@
                         <label for="telp" class="form-label">Telp.</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-telephone"></i></span>
-<<<<<<< HEAD
                             <input type="text" class="form-control" id="telp" name="telp"
                                 placeholder="Nomor Telepon">
-=======
-                            <input type="text" class="form-control" id="telp" placeholder="Nomor Telepon">
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="website" class="form-label">Website</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-globe"></i></span>
-<<<<<<< HEAD
                             <input type="text" class="form-control" id="website" name="website"
                                 placeholder="Alamat Website">
                         </div>
@@ -76,17 +59,6 @@
                     </div>
                 </form>
             </div>
-=======
-                            <input type="text" class="form-control" id="website" placeholder="Alamat Website">
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-success btn-sm"><i class="bx bx-save"></i> Simpan</button>
-                <button type="button" class="btn btn-light btn-sm" data-bs-dismiss="modal">Close</button>
-            </div>
->>>>>>> a08391c349c428e5762a699e9b0a3feb4c7f8e72
         </div>
     </div>
 </div>
